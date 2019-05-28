abee3d25d998f1bbf3606c44d5673f219e572ae4
benjamingarciap <bengarc@gmail.com>
2019-05-28 17:35:03 +0200

    artista y galeria

--- starter-code/routes/artists.routes.js
+++ starter-code/routes/artists.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router  = express.Router()
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const Artwork = require('../models/artwork')
 const User= require('../models/user')
 const cloudinaryConfig = require('../config/cloudinary.config')
 
@@ -18,13 +19,16 @@ router.get('/list',(req, res)=>{
 
 router.get('/detail/:artist_id',(req,res)=>{
   console.log(req.params.user_id)
-  User.find()
+  User.findById(req.params.artist_id)
+  .populate("artworks")
     .then((artist) => {
-      let thisArtist = artist.find(thisArtist => thisArtist._id == req.params.artist_id)
-      //console.log(`Este es el artist ${thisArtist}`)
-      res.render('artist/artist-description', {thisArtist})
-    })
-    .catch(error => console.log(error))
+      //let artworksIds = artist.artworks
+        console.log("estos son los artworks ==>" + artist.artworks)
+        //let thisArtist = artist.find(thisArtist => thisArtist._id == req.params.artist_id)
+        //console.log(`Este es el artist ${thisArtist}`)
+        res.render('artist/artist-description', {artist})
+      })
+         .catch(error => console.log(error))
 })
 
 
--- starter-code/routes/galleries.routes.js
+++ starter-code/routes/galleries.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router  = express.Router()
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
-const User= require('../models/user')
+const User = require('../models/user')
+const Artwork = require("../models/artwork")
 const cloudinaryConfig = require('../config/cloudinary.config')
 
 //LISTADO DE LOS GALERÍA
@@ -13,13 +14,28 @@ router.get('/list',(req, res)=>{
       })
   })
   
-  //VISTA DE CADA GALERÍA
+  // //VISTA DE CADA GALERÍA
   
+  // router.get('/detail/:gallery_id',(req,res)=>{
+  //   User.find(req.params.user_id)
+  //     .then(theGallery=>res.render('gallery/gallery-description', {gallery : theGallery}))
+  //     .catch(error => console.log(error))
+  // })
+
   router.get('/detail/:gallery_id',(req,res)=>{
-    User.find(req.params.user_id)
-      .then(theGallery=>res.render('gallery/gallery-description', {gallery : theGallery}))
-      .catch(error => console.log(error))
+    console.log(req.params.user_id)
+    User.findById(req.params.gallery_id)
+    .populate("artworks")
+      .then((gallery) => {
+        //let artworksIds = artist.artworks
+          console.log("estos son los artworks ==>" + gallery.artworks)
+          //let thisArtist = artist.find(thisArtist => thisArtist._id == req.params.artist_id)
+          //console.log(`Este es el artist ${thisArtist}`)
+          res.render('gallery/gallery-description', {gallery})
+        })
+           .catch(error => console.log(error))
   })
-
-
+  
 module.exports = router
+
+
